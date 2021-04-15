4ad41ac8d1cb0411dcf5492328581be6421606a5
Kasper Buur Vistesen <82087975+kasperbuurDK@users.noreply.github.com>
2021-04-15 17:11:41 +0200

    Update gpioScript.sh

--- gpioScript.sh
+++ gpioScript.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash.
-
-echo ----- Trying wget
-wget https://github.com/joan2937/pigpio/archive/master.zip
-echo ------- after wget
+echo ----- updating python, making sure everything is ok
+sudo apt install python-setuptools python3-setuptools
+echo -------- python updated
+echo ------- cloning pigpio
+git clone https://github.com/joan2937/pigpio
+echo ------- cloned
+echo --------- cd pigpio-master 
+cd pigpio-master
+echo --------- make
+make
+echo --------- make install
+sudo make install
