7d6a6eb815b835cb75c47f28c001d5dc5aecda77
Kasper Buur Vistesen <82087975+kasperbuurDK@users.noreply.github.com>
2021-04-15 15:14:01 +0200

    Update startUpScript.bash

--- startUpScript.bash
+++ startUpScript.bash
@@ -2,6 +2,8 @@
 
 #upgrade and update all
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade 
+sudo apt-get install cmake
+sudo apt-get install git-core
 
 # some setup and test forpigpio library
 sudo apt install python-setuptools python3-setuptools
@@ -14,26 +16,9 @@ cd pigpio-master
 make
 sudo make install
 
-sudo ./x_pigpio # check C I/F
-
-sudo pigpiod    # start daemon
-
-./x_pigpiod_if2 # check C      I/F to daemon
-./x_pigpio.py   # check Python I/F to daemon
-./x_pigs        # check pigs   I/F to daemon
-./x_pipe        # check pipe   I/F to daemon
-
-sudo killall pigpiod  #end pigpiod 
-
-
-
 #preparing for steppermotor library for c++
-
 cd ~
 mkdir stepperMotorDir && cd stepperMotorDir
-sudo apt-get install cmake
-sudo apt-get install git-core
-
 # Download the code
 git clone https://crish4cks@bitbucket.org/crish4cks/steppermotor.git
 
