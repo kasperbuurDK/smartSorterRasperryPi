626c2970ad17ad4352ae5db1f72c741172882ef9
Kasper Buur Vistesen <82087975+kasperbuurDK@users.noreply.github.com>
2021-04-15 15:50:20 +0200

    Update startUpScript.bash

--- startUpScript.bash
+++ startUpScript.bash
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash.
 
 #upgrade and update all
+printf("-------- Updating all ---------- ", \x1B[32m);
+
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade 
 sudo apt-get install cmake
 sudo apt-get install git
 
 
-# some setup and test forpigpio library
+# install python and pigpio
 sudo apt install python-setuptools python3-setuptools
 
 wget https://github.com/joan2937/pigpio/archive/master.zip
