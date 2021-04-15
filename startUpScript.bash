df13c196494330d95d21111f20199792d1a3a736
Kasper Buur Vistesen <82087975+kasperbuurDK@users.noreply.github.com>
2021-04-15 15:44:43 +0200

    Update startUpScript.bash

--- startUpScript.bash
+++ startUpScript.bash
@@ -4,30 +4,18 @@
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade 
 sudo apt-get install cmake
 sudo apt-get install git
-sudo apt-get install wiringpi
+
 
 # some setup and test forpigpio library
 sudo apt install python-setuptools python3-setuptools
 
-cd ~
-mkdir downloadDir && cd downloadDir
 wget https://github.com/joan2937/pigpio/archive/master.zip
 unzip master.zip
 cd pigpio-master
 make
 sudo make install
 
-#preparing for steppermotor library for c++
-cd ~
-mkdir stepperMotorDir && cd stepperMotorDir
-# Download the code
-git clone https://crish4cks@bitbucket.org/crish4cks/steppermotor.git
 
-# Build it!
-cd steppermotor/ 
-mkdir build && cd build
-cmake .. 
-make
 
 
 
