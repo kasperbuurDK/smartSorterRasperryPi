2a75fa14d731534074c73dd07dbce8c26e085945
Kasper Buur Vistesen <82087975+kasperbuurDK@users.noreply.github.com>
2021-04-16 20:51:41 +0200

    Update updateScript.sh

--- updateScript.sh
+++ updateScript.sh
@@ -3,3 +3,6 @@
 #upgrade and update all
 echo '-------- Updating all ----------'
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade 
+
+sudo apt install python-setuptools python3-setuptools
+sudo apt install python3-gpiozero
